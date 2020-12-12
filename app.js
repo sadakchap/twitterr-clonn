7008d8751174f8f90b31f4ea0589d8f7ff62eee9
sadakchap <aliceprerna@gmail.com>
2020-12-12 11:14:37 +0530

    connecting to DB

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
+const mongoose = require("mongoose");
+const { MONGO_URI } = require("./config");
 
 const typeDefs = gql`
   type Query {
@@ -14,8 +16,20 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-const PORT = process.env.PORT || 1930;
-
-server.listen(PORT).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log(`🥳 DB CONNECTED AT ${conn.connection.port}`);
+    const PORT = process.env.PORT || 1930;
+    server.listen(PORT).then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("DB CONNECTION FAILED 🤡🤡");
+    // process.exit(1);
+  });
