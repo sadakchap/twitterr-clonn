d150bd28eea903e826be985e83db45bc654e36dc
sadakchap <aliceprerna@gmail.com>
2020-12-18 17:11:11 +0530

    removed console.log

--- graphql/resolvers/mergerFunction.js
+++ graphql/resolvers/mergerFunction.js
@@ -3,7 +3,6 @@ const User = require("../../models/User");
 const getUser = async (userId) => {
   try {
     const user = await User.findById(userId);
-    console.log(user);
     return {
       ...user._doc,
       id: user._id,
