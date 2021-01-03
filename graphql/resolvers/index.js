d399b973536df0c0963c9043d7666f465a42affe
sadakchap <aliceprerna@gmail.com>
2021-01-03 12:01:18 +0530

    changing messages createdAt to ISOString

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,6 +4,9 @@ const commentResolvers = require("./comment");
 const messageResolvers = require("./message");
 
 module.exports = {
+  Message: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+  },
   Post: {
     likesCount: async (parent) => parent.likes.length,
     commentsCount: async (parent) => parent.comments.length,
