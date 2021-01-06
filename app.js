827886bcbea5222770e501a5de0b1a139519adc7
sadakchap <aliceprerna@gmail.com>
2021-01-06 11:46:06 +0530

    updated context middleware to decode token for websocket links

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const resolvers = require("./graphql/resolvers/index");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req }),
+  context: ({ req, connection }) => ({ req, connection }),
 });
 
 mongoose
--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,22 +1,22 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, PubSub } = require("apollo-server");
 const jwt = require("jsonwebtoken");
 const { JWT_AUTH } = require("../config");
+const pubsub = new PubSub();
 
 module.exports = (context) => {
-  const authHeader = context.req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split("Bearer ")[1];
-    if (token) {
-      try {
-        const decodedToken = jwt.verify(token, JWT_AUTH);
-        return decodedToken;
-      } catch (err) {
-        throw new AuthenticationError("Token invalid/expired!");
-      }
+  let token;
+  if (context.req && context.req.headers.authorization) {
+    token = context.req.headers.authorization.split("Bearer ")[1];
+  } else if (context.connection && context.connection.context.Authorization) {
+    token = context.connection.context.Authorization.split("Bearer ")[1];
+  }
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, JWT_AUTH);
+      return { user: decodedToken, pubsub };
+    } catch (err) {
+      throw new AuthenticationError("Token invalid/expired!");
     }
-    throw new AuthenticationError(
-      `Token must be provided "Bearer token" format`
-    );
   }
   throw new AuthenticationError(`Authorization header is missing!`);
 };
